b715dbb5efa9d7236a5f6dfc2d128b0dc27f5507
Bryce-lazarto1 <blazarto24@gmail.com>
2023-10-03 11:13:56 -0400

    assignment 2.5

--- Assignment 2.5/double_int_list_practice.py
+++ Assignment 2.5/double_int_list_practice.py
@@ -27,7 +27,27 @@ def main():
 
 
   #do each of the four equations with different numbers each time.
+  # Convert the string into a list of integers
 
+
+  # Perform different equations with different numbers
+  problem1 = fiveNumbers[1] + fiveNumbers[3]
+  problem2 = fiveNumbers[5] - fiveNumbers[1]
+  problem3 = fiveNumbers[3] * fiveNumbers[2]
+  problem4 = fiveNumbers[2] / fiveNumbers[1]
+
+  # Print the results of the equations
+  print("Answer of problem 1:", problem1)
+  print("Answer of problem 2:", problem2)
+  print("Answer of problem 3:", problem3)
+  print("Answer of problem 4:", problem4)
+
+  # Replace two numbers in the list
+  numbers[1] = 4
+  numbers[3] = 2
+
+  # Print the updated list
+  print("Updated list:", fiveNumbers)
   #now replace two of the numbers in the list with a different number (using name of list[x] = ?, not rewriting the fiveNumber list)
   
   #print out the list
