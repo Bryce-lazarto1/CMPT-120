4dde975bb634a9ffb6980a7c60aa06f42587c6d5
Bryce-lazarto1 <blazarto24@gmail.com>
2023-11-16 17:44:23 -0500

    assignment 6

--- Assignment 6/Debug.py
+++ Assignment 6/Debug.py
@@ -8,13 +8,12 @@ class Employee:
         self.name = name
         self.idNumber = idNumber
         self.department = department
-    class Cake:
-        def __init__(self, flavor, frosting):
-            self.flavor = flavor
-            self.frosting = frosting
+class Cake:
+    def __init__(self, flavor, frosting):
+        self.flavor = flavor
+        self.frosting = frosting
 
-        def display_info(self):
-            print(f"Cake Flavor: {self.flavor}, Frosting: {self.frosting}")
+
     #can you fill out the rest of this for me? im dumb
     #the cake needs to have the cake flavor and cake frosting stored
 
@@ -38,40 +37,9 @@ class Car:
         self.color = color
         
     #create your own function! what do you want it to do?
-    class Car:
-        def display_info(self):
-            print(f"Car Model: {self.model}, Year: {self.year}, Color: {self.size}")
-
-def main():
-    newDog = Dog("Buddy", 3)
-    print(newDog.name, newDog.age)
-
-    newEmployee = Employee("John Doe", 12345, "IT")
-    print(newEmployee.name, newEmployee.idNumber, newEmployee.department)
-
-    cake1 = Cake("Chocolate", "Vanilla")
-    print("Cake 1:")
-    cake1.display_info()
-
-    cake2 = Cake("double choco;ate", "strawberry")
-    print("Cake 2:")
-        cake2.display_info()
-
-        cat1 = Cat("Whiskers", 2, "long")
-        cat2 = Cat("Mittens", 1, "short")
-
-        print(f"{cat1.name}'s breed guess: {cat1.breedGuess()}")
-
-
-car1 = Car("KIA", 2023, "grey")
-
-print("Car information:")
-car1.display_info()
-
-if __name__ == "__main__":
-    main()
-
-
-        main()
-
+class fish:
+    def __init__(self, type, age, color):
+        self.model = type
+        self.year = age
+        self.color = color
 main()
