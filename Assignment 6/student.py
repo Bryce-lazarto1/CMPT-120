b61fd34aff90d942e62556692add336ff989e9c0
Bryce-lazarto1 <blazarto24@gmail.com>
2023-11-16 17:50:17 -0500

    assignment 6

--- Assignment 6/student.py
+++ Assignment 6/student.py
@@ -23,37 +23,21 @@ create a function because this college is a wacky one- every day they generate a
 4. if not, print "Loser!"
 (disclosure: obviously there's a very small chance of your generated number matching the student id number. I just want to see that you're generating and comparing properly)
 '''
+class Student:
+    def __init__(self, name, studentid, year, major, gpa):
+        self.name = name
+        self.student_id = studentid
+        self.year = year
+        self.major = major
+        self.gpa = gpa
 
-class student:
-    import random
+if gpa >= 3.5:
+    return ("true")
+        print ("true")
+else:
+    return ("false")
+        print("false")
 
-    class Student:
-        def __init__(self, name, student_id, year, major, gpa):
-            self.name = name
-            self.student_id = student_id
-            self.year = year
-            self.major = major
-            self.gpa = gpa
-        def is_eligible_for_honors(self):
-            if self.gpa > 3.5:
-                print(f"{self.name} eligible for the honors program")
-                return True
-            else:
-                print(f"{self.name} not eligible for the honors program.")
-                return False
-        def check_free_lunch(self, random_id):
-            if random_id == self.student_id:
-                print(f"Winner! {self.name} gets free lunch today")
-            else:
-                print("Loser")
-
-
-    Bryce = Student("Bryce Lazarto", 12345, "Freshman", "Computer Science", 3.4)
-    Bryce.is_eligible_for_honors()
-    random_id = random.randint(1000, 9999)
-    Bryce.check_free_lunch(random_id)
-
-    
     
     
     
