3ea835131e02f55990079ea8d797a140b5ec37eb
Bryce-lazarto1 <blazarto24@gmail.com>
2023-11-26 03:30:38 -0500

    assignment

--- Assignment 5/Debug.py
+++ Assignment 5/Debug.py
@@ -7,10 +7,10 @@ def main():
         print("not string :(")
         
     #can you google and find what function you should use to check if it's numeric (an int?)?
-
+'''isnumeric()'''
 
     #what about if it's both letters and numbers?
-
+'''isdigit()'''
 
 
     #good working example
@@ -21,11 +21,25 @@ def main():
         print("not asterisk :(")
         
     #now write code to check if the input was either an asterisk OR an ampersand (&)
-
+ampersand = input("Enter an ampersand please")
+    if ampersand == "&":
+        print("good!")
+    else:
+        print("not ampersand :(")
         
     #do the live example we did in class: ask user to input an integer, but before you cast it to an int, check that it's an integer before doing your variable = int(variable) command
-
+Integer= input("enter a integer")
+if Integer.isdigit():
+    Integer= int(Integer)
+else:
+    print("incorrect")
+print("correct")
     # last challenge: find out how to check if the string input has the substring "marist"
     #google this one ;) substring is the key google term
+Input1 = input("type in marist")
 
+if "marist" in Input1:
+    print("it does have the substring marist.")
+else:
+    print("does not have the sub string marist.")
 main()
--- Assignment 5/int_string_casting_issues.py
+++ Assignment 5/int_string_casting_issues.py
@@ -8,5 +8,7 @@ You can also go much much simpler if you want to, I just want you guys to keep p
 and ofc, if you're stuck, don't hesitate to email
 '''
 def main():
+    try:
+    IntInput = int(input("Enter an int (but enter a string to see the error)"))
 
 main()
